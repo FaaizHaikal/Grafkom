cd05d385cae870e3776f091c0ba53d2c55c4de1d
FaaizHaikal <faaizhaikal@gmail.com>
2024-11-27 15:25:34 +0700

    fix: fix shadow

--- FP/script.js
+++ FP/script.js
@@ -1,29 +1,26 @@
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100);
-
-// load GLB File
-let loader = new THREE.GLTFLoader();
-let object;
-loader.load('./assets/BantayoPoboide1.glb', function(gltf){
-  object = gltf.scene;
-  scene.add(object);
-});
-
 camera.position.set(0, 0, 30);
 
-let renderer = new THREE.WebGLRenderer();
+let loader = new THREE.GLTFLoader();
+loader.load('./assets/BantayoPoboide1.glb', function (gltf) {
+    let object = gltf.scene;
+    scene.add(object);
+});
 
+let renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0xffffff);
+renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
-// let ambientLight = new THREE.AmbientLight(0xffffff, 1.0);
-// scene.add(ambientLight);
+let ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // Ambient lighting
+scene.add(ambientLight);
 
-let light = new THREE.DirectionalLight(0xffffff, 1.0);
-light.position.set(100, 100, -100);
-scene.add(light);
-
+let directionalLight = new THREE.DirectionalLight(0xffffff, 1.0); // Main light
+directionalLight.position.set(100, 100, -100);
+directionalLight.castShadow = true;
+scene.add(directionalLight);
 
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
@@ -31,8 +28,7 @@ controls.dampingFactor = 0.25;
 controls.enableZoom = true;
 
 function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
 }
-
 animate();
